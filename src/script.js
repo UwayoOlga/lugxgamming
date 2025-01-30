fe1f595ccf7d8102add61fb6ff48478b159e350a
UwayoOlga <uwayoolga@gmail.com>
2025-01-30 22:22:25 +0200

    scroll class selector gone wrong :(

---
+++ src/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');  
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) {  
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
