11055ae65323be690e101eca2006b19bf6c653a3
UwayoOlga <uwayoolga@gmail.com>
2025-02-03 22:23:37 +0200

    home page font adjustments

--- src/script.js
+++ src/script.js
@@ -1,10 +1,11 @@
+const navbar = document.getElementById('myNavbar');
+  
 window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header');  
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition > 100) {  
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
+  if (window.scrollY > 0) {
+    navbar.classList.remove('bg-transparent');
+    navbar.classList.add('bg-blue-500'); // Or your desired color
+  } else {
+    navbar.classList.remove('bg-blue-500');
+    navbar.classList.add('bg-transparent');
+  }
 });
