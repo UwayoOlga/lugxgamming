5222445c55b93a6699f9719dcde0984248c8e566
UwayoOlga <uwayoolga@gmail.com>
2025-02-04 17:32:18 +0200

    adjusting padding and adding icons

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const navbar = document.getElementById('myNavbar');
 window.addEventListener('scroll', () => {
   if (window.scrollY > 0) {
     navbar.classList.remove('bg-transparent');
-    navbar.classList.add('bg-blue-500'); // Or your desired color
+    navbar.classList.add('bg-blue-500');  
   } else {
     navbar.classList.remove('bg-blue-500');
     navbar.classList.add('bg-transparent');
